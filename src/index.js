6d6eab24227d185b53c79946fe78431f1383946a
mrdekan <yeros05040504@gmail.com>
2023-02-20 20:24:18 +0200

    Some changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import {Client, GatewayIntentBits} from 'discord.js';
 config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.login(process.env.SAYA_BOT_TOKEN);
+client.login(process.env.SAYA_TOKEN);
 client.once('ready', ()=> {
     console.log("I`m online!");
+    console.log(`${client}`);
 });

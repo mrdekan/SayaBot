eb3e35bd65bef1bcecfef95b4bc0419e6f542028
mrdekan <yeros05040504@gmail.com>
2023-02-20 21:20:18 +0200

    Added some simples commands

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import { config } from 'dotenv'
-import {Client, GatewayIntentBits} from 'discord.js';
+import {Client, GatewayIntentBits, Routes} from 'discord.js';
+import { REST } from '@discordjs/rest';
 
 config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.login(process.env.SAYA_TOKEN);
-client.once('ready', ()=> {
-    console.log("I`m online!");
-    console.log(`${client}`);
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ] });
+const TOKEN = process.env.SAYA_TOKEN;
+const CLIENT_ID = process.env.SAYA_ID;
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+client.login(TOKEN);
+client.once('ready', ()=> {console.log("I`m online!")});
+
+async function main() {
+    const commands = [
+        {
+            name: 'ping',
+            description: 'Replies with Pong!',
+        },
+        {
+            name: 'order',
+            description: 'Order something...',
+        },
+    ];
+
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(Routes.applicationCommands(CLIENT_ID), {
+            body: commands,
+        });
+        client.login(TOKEN);
+    } catch (err) {
+        console.log(err);
+    }
+}
+main();
+
